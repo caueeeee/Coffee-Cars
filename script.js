90fd76cf8bf2a55218c47fe2989896548022e37b
souzza <cauelima752@gmail.com>
2025-05-23 00:01:48 -0300

    adição de animação gsap nas divs

---
+++ script.js
@@ -0,0 +1,91 @@
+gsap.to(".card-classic", {
+  y: -20,
+  opacity: 1,
+  filter: "blur(0px)",
+  scrollTrigger: {
+    trigger: ".card-classic",
+    start: "top 85%",
+    end: "top 80%",
+    markers: false,
+    duration: 0.5,
+    scrub: 0.1,
+  },
+});
+gsap.to(".card-street", {
+  y: -20,
+  opacity: 1,
+  filter: "blur(0px)",
+  scrollTrigger: {
+    trigger: ".card-street",
+    start: "top 85%",
+    end: "top 80%",
+    markers: false,
+    duration: 0.5,
+    scrub: 0.1,
+  },
+});
+gsap.to(".card-review", {
+  y: -20,
+  opacity: 1,
+  filter: "blur(0px)",
+  scrollTrigger: {
+    trigger: ".card-review",
+    start: "top 85%",
+    end: "top 80%",
+    markers: false,
+    duration: 0.5,
+    scrub: 0.1,
+  },
+});
+gsap.to(".card-hypercars", {
+  y: -20,
+  opacity: 1,
+  filter: "blur(0px)",
+  scrollTrigger: {
+    trigger: ".card-hypercars",
+    start: "top 85%",
+    end: "top 80%",
+    markers: false,
+    duration: 0.5,
+    scrub: 0.1,
+  },
+});
+gsap.to(".card-records", {
+  y: -20,
+  opacity: 1,
+  filter: "blur(0px)",
+  scrollTrigger: {
+    trigger: ".card-records",
+    start: "top 85%",
+    end: "top 80%",
+    markers: false,
+    duration: 0.5,
+    scrub: 0.1,
+  },
+});
+gsap.to(".card-success", {
+  y: -20,
+  opacity: 1,
+  filter: "blur(0px)",
+  scrollTrigger: {
+    trigger: ".card-success",
+    start: "top 85%",
+    end: "top 80%",
+    markers: false,
+    duration: 0.5,
+    scrub: 0.1,
+  },
+});
+
+gsap.to(".hero", {
+    marginTop:"-50px",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".dtq",
+      start: "top 85%",
+      end: "top 50%",
+      markers: false,
+      duration: 0.5,
+      scrub: 0.1,
+    },
+})
